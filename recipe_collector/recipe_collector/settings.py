7aa5fdbc1785e365b8c97b1c778c60f9b302053e
jekat <pulpy231@gmail.com>
2025-01-15 19:22:58 +0200

    corrected settings.py

--- recipe_collector/recipe_collector/settings.py
+++ recipe_collector/recipe_collector/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,9 @@ ROOT_URLCONF = 'recipe_collector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'authentication/templates/authentication',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,10 +75,18 @@ WSGI_APPLICATION = 'recipe_collector.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'bar000',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
+
 }
 
 
