4c69c4599ebff43fc35de9a5b94fce0776cbea96
jekat <pulpy231@gmail.com>
2025-01-17 12:40:23 +0200

    corrected base settings: added os template variabes

--- recipe_collector/recipe_collector/settings.py
+++ recipe_collector/recipe_collector/settings.py
@@ -1,8 +1,8 @@
+import os
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -40,7 +40,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'authentication/templates/authentication',
+            os.path.join(BASE_DIR,'authentication/templates/authentication'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
