b3f1ab89343b1c90e02cc17f773eabdc2dc35025
jekat <pulpy231@gmail.com>
2025-01-26 03:20:39 +0200

    fixed tests

--- favourite_tastebook/authentication/tests.py
+++ favourite_tastebook/authentication/tests.py
@@ -18,41 +18,43 @@ class AuthenticationTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertTrue(User.objects.filter(username='testuser123').exists())
         self.assertRedirects(response, reverse('home_page'))
-        self.assertEqual(response.status_code, 404)
 
 
-    def test_register_view_post_invalid_credentials(self):
+    def test_register_view_post_invalid_password_credentials(self):
         response = self.client.post(reverse('register'), {
             'username': 'invalid-testuser123',
-            'password': 'baba12345',
-            'confirm_password': 'baba12345',
+            'password1': 'baba123',
+            'password2': 'baba1234567',
         })
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'The two password fields didn’t match.')
         self.assertFalse(User.objects.filter(email='invalid-testuser123').exists())
 
 
-    def test_register_view_invalid_credentials(self):
-        self.user = User.objects.create_user(username='testuser')
-
-
     def test_login_view(self):
+        User.objects.create_user(username='testuser', password='password123')
+
         response = self.client.post(reverse('login'), {
             'username': 'testuser',
             'password': 'password123'
         })
+
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, reverse('home_page'))
+        self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_logout_view(self):
-        response = self.client.post(reverse('login'))
-        self.assertEqual(response.status_code, 200)
 
     def test_login_invalid_credentials_view(self):
+        User.objects.create_user(username='testuser', password='password123')
 
         response = self.client.get(reverse('login'), {
-            'username': 'wrong_user',
-            'password': 'password123456_wrong',
+            'username': 'testuser',
+            'password': 'password123_wrong',
         })
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_logout_view(self):
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
 
