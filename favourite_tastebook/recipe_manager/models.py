a7ea757beb5663cb1f7f7240aaa0852cb885eb10
jekat <pulpy231@gmail.com>
2025-01-29 23:28:57 +0200

    created models

---
+++ favourite_tastebook/recipe_manager/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
+    tags = models.CharField(max_length=100, null=True, blank=True)
+    instructions = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    cook_time = models.IntegerField(help_text = "Time in minutes", null=True, blank=True)
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='dishes')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class UserIngredients(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_ingredients')
