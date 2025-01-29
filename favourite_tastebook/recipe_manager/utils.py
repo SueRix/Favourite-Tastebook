0ca25e88059cccdf8081bdc959a063b1540e42a5
jekat <pulpy231@gmail.com>
2025-01-30 00:19:32 +0200

    created 'pass' 4 defs for every import

--- favourite_tastebook/recipe_manager/utils.py
+++ favourite_tastebook/recipe_manager/utils.py
@@ -1,6 +1,10 @@
-# This file needed to reading of .csv files for fill PostgreSQL database.
+import csv
+from django.db import transaction
+from django.contrib.auth.models import User
+from models import Ingredient, Recipe, Dish, UserIngredients
 
-# It is example of kind .csv table for Models:
+# This file needed to reading of .csv files for fill PostgreSQL database.
+# It is examples of kind .csv files for Models:
 
 # ingredients.csv:
 """
@@ -26,3 +30,16 @@ Spaghetti,1,1
 user,ingredients,quantity,unit,created_by
 1,2,500,g,1
 """
+
+#TODO: Create CRUD functions for every model.
+def import_ingredients(file_path):
+    pass
+
+def import_recipes(file_path):
+    pass
+
+def import_dishes(file_path):
+    pass
+
+def import_user_ingredients(file_path):
+    pass
