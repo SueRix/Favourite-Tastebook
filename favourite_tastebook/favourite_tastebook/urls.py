f14c456abcc60e583841a0d2e1ee2e77b9b41939
jekat <pulpy231@gmail.com>
2025-02-01 19:47:37 +0200

    refactored home page into recipe_manager_app and added some first UI page. created urls

--- favourite_tastebook/favourite_tastebook/urls.py
+++ favourite_tastebook/favourite_tastebook/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from recipe_manager.views import IndexView, OurProductView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('authentication.urls')),
-    path('welcome/', IndexView.as_view(), name='welcome'),
-    path('our_product/', OurProductView.as_view(), name='our_product')
+    path('', include('recipe_manager.urls')),
 ]
--- favourite_tastebook/recipe_manager/urls.py
+++ favourite_tastebook/recipe_manager/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from .views import IndexView, OurProductView, HomeView
+
+urlpatterns = [
+    path('welcome/', IndexView.as_view(), name='welcome'),
+    path('our_product/', OurProductView.as_view(), name='our_product'),
+    path('home/', HomeView.as_view(), name='home'),
+]
