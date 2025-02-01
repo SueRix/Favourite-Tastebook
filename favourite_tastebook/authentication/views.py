1f3d03b89754b0d17cc636b0f4d1277abcf4665d
jekat <pulpy231@gmail.com>
2025-02-01 20:32:49 +0200

    created home html. added pretty styles also to another pages

--- favourite_tastebook/authentication/views.py
+++ favourite_tastebook/authentication/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 
@@ -7,4 +8,4 @@ class RegisterView(CreateView):
     model = User
     form_class = UserCreationForm
     template_name = 'register.html'
-    success_url = 'home'
+    success_url = reverse_lazy('home')
--- favourite_tastebook/recipe_manager/urls.py
+++ favourite_tastebook/recipe_manager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import IndexView, OurProductView, HomeView
 
 urlpatterns = [
-    path('welcome/', IndexView.as_view(), name='welcome'),
+    path('', IndexView.as_view(), name='welcome'),
     path('our_product/', OurProductView.as_view(), name='our_product'),
     path('home/', HomeView.as_view(), name='home'),
 ]
