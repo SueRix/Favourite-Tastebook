cc564b514c2668cdfe794c5801ab5c266ec39283
jekat <pulpy231@gmail.com>
2025-02-16 20:59:30 +0200

    fix to info-page

--- favourite_tastebook/recipe_manager/urls.py
+++ favourite_tastebook/recipe_manager/urls.py
@@ -3,7 +3,7 @@ from .views import IndexView, ProductFeaturesView, HomeView, FilterRecipesView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='welcome'),
-    path('product_features/', ProductFeaturesView.as_view(), name='our_product'),
+    path('product_features/', ProductFeaturesView.as_view(), name='product_features'),
     path('home/', HomeView.as_view(), name='home'),
     path('api/filter_recipes/', FilterRecipesView.as_view(), name='filter_recipes'),
 
