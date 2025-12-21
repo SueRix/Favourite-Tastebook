31b8e11ca1eb96edb9cdb661bba1bee69a377be6
suerix <pulpy231@gmail.com>
2025-12-22 00:39:43 +0100

    refactored views to resolve ide problems

--- favourite_tastebook/profile_manager/views.py
+++ favourite_tastebook/profile_manager/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.http import Http404
+from django.http import Http404, HttpRequest
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import DetailView, View
+from django.views.generic import DetailView, View, TemplateView
 
-from .exceptions import ProfileNotFound, InvalidFormData
+from .exceptions import ProfileNotFound
 from .forms import ProfileForm, UserUpdateForm
 from .models import Profile
-from django.views.generic import TemplateView
 
 
 class ProfileDetailView(DetailView):
@@ -23,6 +22,8 @@ class ProfileDetailView(DetailView):
 
 
 class OwnerRequiredMixin(UserPassesTestMixin):
+    request: HttpRequest
+    kwargs: dict
 
     def test_func(self):
         username = self.kwargs.get("username")
@@ -37,57 +38,63 @@ class OwnerRequiredMixin(UserPassesTestMixin):
 class ProfileUpdateView(LoginRequiredMixin, OwnerRequiredMixin, View):
     template_name = "profile/edit.html"
 
-    def _get_forms(self, request):
+    @staticmethod
+    def _get_forms(request, user):
         try:
-            profile = request.user.profile
+            profile = user.profile
         except Profile.DoesNotExist as exc:
             raise ProfileNotFound("Profile object not found for current user.") from exc
 
         if request.method == "POST":
-            user_form = UserUpdateForm(request.POST, instance=request.user)
+            user_form = UserUpdateForm(request.POST, instance=user)
             profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
         else:
-            user_form = UserUpdateForm(instance=request.user)
+            user_form = UserUpdateForm(instance=user)
             profile_form = ProfileForm(instance=profile)
         return user_form, profile_form
 
-    def get(self, request, username):
+    def get(self, request, *args, **kwargs):
         try:
-            user_form, profile_form = self._get_forms(request)
+            user_form, profile_form = self._get_forms(request, request.user)
         except ProfileNotFound:
             raise Http404("Profile not found.")
         ctx = {"user_form": user_form, "profile_form": profile_form}
         return render(request, self.template_name, ctx, status=200)
 
-    def post(self, request, username):
+    def post(self, request, *args, **kwargs):
         try:
-            user_form, profile_form = self._get_forms(request)
-            if user_form.is_valid() and profile_form.is_valid():
-                user_form.save()
-                profile_form.save()
-                ctx = {"user_form": UserUpdateForm(instance=request.user),
-                       "profile_form": ProfileForm(instance=request.user.profile),
-                       "saved": True}
-                return render(request, self.template_name, ctx, status=200)
-
-            raise InvalidFormData(user_errors=user_form.errors, profile_errors=profile_form.errors)
-
-        except InvalidFormData as e:
-            ctx = {
-                "user_form": user_form,
-                "profile_form": profile_form,
-                "errors": {"user": e.user_errors, "profile": e.profile_errors},
-            }
-            return render(request, self.template_name, ctx, status=400)
-
+            user_form, profile_form = self._get_forms(request, request.user)
         except ProfileNotFound:
             raise Http404("Profile not found.")
 
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            ctx = {
+                "user_form": UserUpdateForm(instance=request.user),
+                "profile_form": ProfileForm(instance=request.user.profile),
+                "saved": True
+            }
+            return render(request, self.template_name, ctx, status=200)
+
+        ctx = {
+            "user_form": user_form,
+            "profile_form": profile_form,
+            "errors": {
+                "user": user_form.errors,
+                "profile": profile_form.errors
+            },
+        }
+        return render(request, self.template_name, ctx, status=400)
+
+
 class TastesView(TemplateView):
-    template_name = "profiles/tastes_stub.html"
+    template_name = "profile/tastes_stub.html"
+
 
 class CookbooksView(TemplateView):
-    template_name = "profiles/cookbooks_stub.html"
+    template_name = "profile/cookbooks_stub.html"
+
 
 class SavedRecipesView(TemplateView):
-    template_name = "profiles/saved_stub.html"
+    template_name = "profile/saved_stub.html"
