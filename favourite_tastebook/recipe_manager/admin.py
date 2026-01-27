ab77f49cf8e125bcb12fc17a106a49b05dd1c334
suerix <pulpy231@gmail.com>
2026-01-27 16:11:33 +0100

    admin for image filling fix

--- favourite_tastebook/recipe_manager/admin.py
+++ favourite_tastebook/recipe_manager/admin.py
@@ -4,4 +4,4 @@ from .models import Recipe
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("title", "cuisine")
-    fields = ("title", "cuisine", "description", "image")
+    fields = ("title", "cuisine", "description", "image_url")
