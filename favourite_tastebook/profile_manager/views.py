01ed2e3b1b65dc25089a6dfb19142c8ebaea899d
suerix <pulpy231@gmail.com>
2026-02-03 18:48:37 +0100

    deleted profile stub paths

--- favourite_tastebook/profile_manager/views.py
+++ favourite_tastebook/profile_manager/views.py
@@ -1,9 +1,7 @@
 from django.views import View
 from django.http import HttpRequest, HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
 
 from .handlers.profile import profile_detail, profile_edit
 
@@ -11,23 +9,10 @@ from .handlers.profile import profile_detail, profile_edit
 @method_decorator(login_required, name="dispatch")
 class ProfileDetailView(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        return profile_detail(request)
+        return profile_detail(self.request)
 
 
 @method_decorator(login_required, name="dispatch")
 class ProfileUpdateView(View):
     def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return profile_edit(request)
-
-
-
-class TastesView(LoginRequiredMixin, TemplateView):
-    template_name = "profile/tastes_stub.html"
-
-
-class CookbooksView(LoginRequiredMixin, TemplateView):
-    template_name = "profile/cookbooks_stub.html"
-
-
-class SavedRecipesView(LoginRequiredMixin, TemplateView):
-    template_name = "profile/saved_stub.html"
