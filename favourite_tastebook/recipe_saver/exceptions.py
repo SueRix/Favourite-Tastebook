a7312ab0de6d63b9e48bd79716adfedc2f844653
suerix <pulpy231@gmail.com>
2026-02-03 17:08:15 +0100

    magic method for exce

--- favourite_tastebook/recipe_saver/exceptions.py
+++ favourite_tastebook/recipe_saver/exceptions.py
@@ -1,6 +1,9 @@
 class SavedRecipeException(Exception):
     message = "An error occurred while processing saved recipes."
 
+    def __init__(self, message=None):
+        super().__init__(message or self.message)
+
 class RecipeAlreadySavedError(SavedRecipeException):
     message = "This recipe is already in your saved list."
 
