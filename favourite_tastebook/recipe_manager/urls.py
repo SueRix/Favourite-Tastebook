60a031752fd0a58776b86bc2faa56a759c80bce1
suerix <pulpy231@gmail.com>
2026-03-06 17:50:34 +0100

    cleared urls

--- favourite_tastebook/recipe_manager/urls.py
+++ favourite_tastebook/recipe_manager/urls.py
@@ -1,24 +1,24 @@
 from django.urls import path
 
-from .views import (
+from .views.main_views import (
     MainTastebookView,
-    IngredientsPartialView,
-    SelectedIngredientsPartialView,
-    RecipesPartialView, IngredientSearchView,
-    SavedRecipeListView, SavedRecipeActionView,
     TastesView,
-    CookbooksView,
+)
+from .views.recipe_manager_views import (
+    IngredientsPartialView,
+    RecipesPartialView, IngredientSearchView,
+)
+from .views.recipe_saver_views import (
+    SavedRecipeListView, SavedRecipeActionView,
 )
 
 urlpatterns = [
     path("", MainTastebookView.as_view(), name="home"),
 
-    path("partials/ingredients/", IngredientsPartialView.as_view(), name="partials_ingredients"),
-    path("partials/selected/", SelectedIngredientsPartialView.as_view(), name="partials_selected"),
+    path("partials/ingredients/", IngredientsPartialView.as_view(), name="partials_ingredients_panel"),
     path("partials/recipes/", RecipesPartialView.as_view(), name="partials_recipes"),
     path('ingredients/search/', IngredientSearchView.as_view(), name='ingredient_search_results'),
     path('saved/', SavedRecipeListView.as_view(), name='saved_recipes'),
     path('saved/<int:recipe_id>/', SavedRecipeActionView.as_view(), name='saved_recipe_action'),
     path("tastes/", TastesView.as_view(), name="tastes"),
-    path("cookbooks/", CookbooksView.as_view(), name="cookbooks"),
 ]
