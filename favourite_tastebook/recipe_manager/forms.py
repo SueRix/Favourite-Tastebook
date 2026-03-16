aebbaf56cdd98e7ada54555e327f6c823b5de0db
suerix <pulpy231@gmail.com>
2026-03-17 00:41:00 +0100

    fix form auto prompting

--- favourite_tastebook/recipe_manager/forms.py
+++ favourite_tastebook/recipe_manager/forms.py
@@ -21,34 +21,8 @@ class RecipeSearchForm(forms.Form):
 class ImageAIAnalysisForm(forms.Form):
     image = forms.ImageField(
         required=True,
-        error_messages={'required': 'An image of the fridge is required.'}
+        error_messages={'required': 'An image of the ingredients is required.'}
     )
-
-    ingredients = forms.CharField(required=True)
-
-    def clean_ingredients(self) -> list:
-        raw_ingredients = self.cleaned_data.get('ingredients')
-        try:
-            parsed_list = json.loads(raw_ingredients)
-
-            if not isinstance(parsed_list, list):
-                raise ValidationError("Ingredients must be a JSON array.")
-
-            names_list = []
-            for item in parsed_list:
-                if isinstance(item, str):
-                    names_list.append(item)
-                elif isinstance(item, dict) and "fields" in item and "name" in item["fields"]:
-                    names_list.append(item["fields"]["name"])
-                elif isinstance(item, dict) and "name" in item:
-                    names_list.append(item["name"])
-                else:
-                    raise ValidationError("Array items must be strings or objects with a 'name' field.")
-
-            return names_list
-
-        except json.JSONDecodeError:
-            raise ValidationError("Invalid JSON format for ingredients.")
 
     def get_base64_image(self) -> str:
         image_file = self.cleaned_data.get('image')
