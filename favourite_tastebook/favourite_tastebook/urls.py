5322aac45b356675065a876af2ed13317b737b70
suerix <pulpy231@gmail.com>
2026-06-08 14:34:24 +0200

    gcp deployment docker and settings

--- favourite_tastebook/favourite_tastebook/urls.py
+++ favourite_tastebook/favourite_tastebook/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.urls import re_path
+from django.views.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -11,6 +12,8 @@ urlpatterns = [
     path("profile/", include("profile_manager.urls")),
 
     path("home/", include("recipe_manager.urls")),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
